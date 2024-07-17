b4cd511d19cb664ef00edb3157139677c3e12f7e
rohitpol0911@gmail.com <rohitpol0911@gmail.com>
2024-07-17 14:34:16 +0530

    hdfifigf

--- Typescript/4_Index.js
+++ Typescript/4_Index.js
@@ -1,13 +1,4 @@
 "use strict";
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
-    }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 var _1_Prepare_1 = require("./1_Prepare");
 // const a1=Fields.Details;
@@ -26,6 +17,8 @@ console.log(pos);
 // console.log(pos1);
 // console.log(pos2);
 // const updated=Fields.Measures.filter((x)=>x.entityName!='Discount')
-var updated = __spreadArray(__spreadArray([], _1_Prepare_1.fieldList.slice(0, pos), true), _1_Prepare_1.fieldList.slice(pos + 1), true);
-console.log(updated.length);
+// const updated=[...fieldList.splice(0,pos), ...fieldList.splice(pos+1)]
+_1_Prepare_1.fieldList.splice(pos, 1);
+// console.log(updated.length);
 console.log(_1_Prepare_1.fieldList.length);
+console.log(_1_Prepare_1.fieldList);
--- Typescript/4_Index.ts
+++ Typescript/4_Index.ts
@@ -32,16 +32,22 @@ console.log(pos);
 // console.log(pos2);
 
 
-
+/* DEleting using Filter
 // const updated=Fields.Measures.filter((x)=>x.entityName!='Discount')
 
 
+Deleting using Slice: Where we need to create another array to store the changes
+// const updated=[...fieldList.splice(0,pos), ...fieldList.splice(pos+1)]
 
-const updated=[...fieldList.slice(0,pos), ...fieldList.slice(pos+1)]
+*/
+
+// SPLICE : Here we can make changes into original array  
+fieldList.splice(pos,1);
 
 // console.log(updated.length);
-// console.log(fieldList.length);
+console.log(fieldList.length);
 
-console.log(updated);
+console.log(fieldList);
+
 
 
